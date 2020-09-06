875c7980c9fa720668cd13660188e3ebca2c5fb3
A Bond <a.rosicbond@gmail.com>
2020-09-06 16:04:06 +0100

    flexible list component practice

--- src/App.js
+++ src/App.js
@@ -1,71 +1,91 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Aldo', age: 45 },
-      { name: 'Jacksie', age: 22.5 },
-      { name: 'John', age: 11.25 }
+      {id: 'sdfdsfds', name: "Aldo", age: 45 },
+      {id: 'dcdscsd', name: "Jacksie", age: 22.5 },
+      {id: 'scsdcsd', name: "John", age: 11.25 },
     ],
-    otherState: 'some other value'
+    otherState: "some other value",
+    showPersons: false,
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Big Aldo';
-    this.setState({
-      persons: [
-        { name: newName, age: 90 },
-        { name: 'Big Jacksie', age: 45 },
-        { name: 'Big John', age: 22.5 }
-      ]
-    });
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Big Aldo';
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 90 },
+  //       { name: "Big Jacksie", age: 45 },
+  //       { name: "Big John", age: 22.5 },
+  //     ],
+  //   });
+  // };
+
+  handleNameChange = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
-  handleNameChange = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Big Aldo', age: 90 },
-        { name: event.target.value, age: 45 },
-        { name: 'Big John', age: 22.5 }
-      ]
-    });
-  }
+  handlePersonToggle = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons : !doesShow })
+  };
+
+  handlePersonDelete = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  };
 
   render() {
     const style = {
-      backgroundColor: 'teal',
-      font: 'inherit',
-      border: '1px solid yellow',
-      padding: '12px',
-      cursor: 'pointer'
+      backgroundColor: "teal",
+      font: "inherit",
+      border: "1px solid yellow",
+      padding: "12px",
+      cursor: "pointer",
     };
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+              click={() => this.handlePersonDelete(index)}
+              name={person.name} 
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.handleNameChange(event, person.id)} />
+            })}
+          </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1>HoWdY pLaNeT</h1>
         <p>HeRe To ParTy Wi ReAcT</p>
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler('Big Aldo')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Big Jacksie!!!')}
-          changed={this.handleNameChange}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <button style={style} onClick={this.handlePersonToggle}>
+          Toggle Persons 
+        </button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
